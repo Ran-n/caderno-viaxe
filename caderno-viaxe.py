997b1b354e62db494276e31b6ab20ae4a9f02141
Ran-n <ran-n@tutanota.com>
2019-06-25 15:59:35 +0200

    gitignore

--- caderno-viaxe.py
+++ caderno-viaxe.py
@@ -6,7 +6,10 @@
 #------------------------------------------------------------------------------------------------
 import json
 from pathlib import Path
-#from functools import partial
+from functools import partial
+#------------------------------------------------------------------------------------------------
+__fnotas = 'media/notas.json'
+__findice = 'media/indice.json'
 #------------------------------------------------------------------------------------------------
 # función encargada de cargar ficheiros tipo json
 def cargar_json(fich):
@@ -16,13 +19,22 @@ def cargar_json(fich):
 		open(fich, 'w').write('{}')
 		return json.loads(open(fich).read())
 #------------------------------------------------------------------------------------------------
-def engadir():
-	print('engadindo')
+def gardar_json(fich, contido):
+	open(fich, 'w').write(json.dumps(contido, indent=1, sort_keys=False))
+#------------------------------------------------------------------------------------------------
+def engadir(indice, cant):
+	media = {'nome': None,
+			'tipo': None}
+
+	print('Nome', end=': ')
+	media['nome'] = input()
+	print('Tipo', end=': ')
+	media['tipo'] = input()
+
+	indice[cant] = media
+
 #------------------------------------------------------------------------------------------------
 def menu():
-	#__ops = {'1': partial(engadir, )}
-	__ops = {'1': engadir}
-
 	while True:
 		print('1 - Engadir Serie/Peli/Docu')
 
@@ -33,17 +45,21 @@ def menu():
 			break
 		else:
 			print('Selecciona unha das posibles.\n')
-	__ops[op]()
+	return op
 #------------------------------------------------------------------------------------------------
 if __name__=="__main__":
-	__fnotas = 'media/notas.json'
-	__findice = 'media/indice.json'
-
 	notas = cargar_json(__fnotas)
 	indice = cargar_json(__findice)
+	cant = len(indice)
+	__ops = {'1': partial(engadir, indice, cant)}
 
-	menu()
+	__ops[menu()]()
 
+	print('Notas')
 	print(json.dumps(notas, indent=1, sort_keys=True))
+	print('Índice')
 	print(json.dumps(indice, indent=1, sort_keys=True))
+
+	gardar_json(__fnotas, notas)
+	gardar_json(__findice, indice)
 #------------------------------------------------------------------------------------------------

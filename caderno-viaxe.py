4ca83b00f8e96f7ef22a0a3d23f87b0b9e26f52f
Ran-n <ran-n@tutanota.com>
2019-06-26 16:12:23 +0200

    falta que se xa existe a review non a sobreescriba

--- caderno-viaxe.py
+++ caderno-viaxe.py
@@ -1,23 +1,29 @@
 #! /usr/bin/python3
 # -*- coding: utf-8 -*-
-
 #+ Autor:	Ran#
 #+ Creado:	25/06/2019 14:27:50
-#+ Editado:	25/06/2019 16:33:56
+#+ Editado:	26/06/2019 16:07:25
 #------------------------------------------------------------------------------------------------
 import json
 from functools import partial
 import utils as u
 import gettext
 #------------------------------------------------------------------------------------------------
-__fnotas = 'media/notas.json'
-__findice = 'media/indice.json'
+__cmedia = 'media/'
+__cnotas = 'media/notas/'
+__cseries = __cnotas+'s/'
+__cpelis = __cnotas+'p/'
+__cdocus = __cnotas+'d/'
+
+__fnotas = __cmedia + 'notas.json'
+__findice = __cmedia + 'indice.json'
 
 __cod_serie = 's'
 __cod_peli = 'p'
 __cod_docu = 'd'
 #------------------------------------------------------------------------------------------------
 def engadir(indice):
+	existe = False
 	media = {'nome': None,
 			'tipo': None}
 
@@ -25,54 +31,105 @@ def engadir(indice):
 	print(_('*> Engadindo:'))
 	print('-----------------------')
 	print(_(' > Nome'), end=': ')
-	media['nome'] = input()
+	nome = input()
 
-	while True:
-		print(_(' > Tipo (1 serie, 2 peli, 3 docu)'), end=': ')
-		tipo = input()
+	for ele in indice.values():
+		if ele['nome'].lower() == nome.lower():
+			existe = True
+			break
 
-		if tipo == '1':
-			media['tipo'] = __cod_serie
-			break
-		elif tipo == '2':
-			media['tipo'] = __cod_peli
-			break
-		elif tipo == '3':
-			media['tipo'] = __cod_docu
-			break
-		else:
-			media['tipo'] = 'imposible'
+	if existe:
+		print(_('*> Xa existe compi'))
+	else:
+		media['nome'] = nome
+
+		# para ver que pon un tipo que exista
+		while True:
+			print(_(' > Tipo (1 serie, 2 peli, 3 docu)'), end=': ')
+			tipo = input()
+
+			# segundo o tipo poñemos o seu código
+			if tipo == '1':
+				media['tipo'] = __cod_serie
+				u.crear_carp(nome)
+				break
+			elif tipo == '2':
+				media['tipo'] = __cod_peli
+				break
+			elif tipo == '3':
+				media['tipo'] = __cod_docu
+				break
+			else:
+				media['tipo'] = 'imposible'
+
+		# engadimos o contido audiovisual á colección xeral
+		indice[len(indice)] = media
+
 	print('-----------------------')
 
-	indice[len(indice)] = media
+	# devolvemos estes valores engadidos se é que se queren
+	return media['nome'], media['tipo']
+#------------------------------------------------------------------------------------------------
+# función que se encarga de coller e retornar os valores a meter no ficheiro de valoración
+def valorar_aux2():
+	valoracion = {'resumo': None,
+				'opinions': None,
+				'nota': None}
+
+	print(_('\nResumo: '))
+	valoracion['resumo'] = input()
+
+	return valoracion
+#------------------------------------------------------------------------------------------------
+# función auxiliar que se encarga do proceso unha vez sabemos nome e tipo do contido
+def valorar_aux(nome, tipo):
+	# miramos o tipo de contido do que se trata
+	if tipo == __cod_serie:
+		# miramos que nos diga un epi válido
+		while True:
+			print(_(' > Que capítulo? (1x01)'), end=': ')
+			cap = input().lower()
+			if u.epi_valido(cap):
+				u.gardar_json(__cseries + nome +'/'+ cap+'.json', valorar_aux2())
+				break
+			else:
+				print(_('*> Capítulo inválido.\n'))
+
+	elif tipo == __cod_peli:
+		u.gardar_json(__cpelis + nome +'.json', valorar_aux2())
+
+	elif tipo == __cod_docu:
+		u.gardar_json(__cdocus + nome +'.json', valorar_aux2())
+
+	# debería ser imposible que non coincida con ningún código
+	else:
+		print(_('Imposible'))
 #------------------------------------------------------------------------------------------------
 def valorar(indice, notas):
+	# variable que determina se crear ou non un material audiovisual no indice
 	crear = True
+
 	print('\n-----------------------')
 	print(_(' > Título do contido'), end=': ')
 	nome = input()
 
-	# primeiro miramos se esta no índice, senon creamolo
+	# primeiro miramos se esta no índice, senon crearemolo
 	for ele in indice.values():
+		# se está no índice
 		if ele['nome'].lower() == nome.lower():
-			if ele['tipo'] == __cod_serie:
-				while True:
-					print(_(' > Que capítulo? (1x01)'), end=': ')
-					cap = input().lower()
-					if u.epi_valido(cap):
-						print('beeen')
-						break
-					else:
-						print(_('*> Capítulo inválido.\n'))
+			# pasamos o proceso á función auxiliar
+			valorar_aux(ele['nome'], ele['tipo'])
 
 			crear = False
 			print('-----------------------')
 			break
 
+	# se non está no indice crearemolo
 	if crear:
-		print(_('*> Non existe, engadir? (si[s]/non[n])'), end=': ')
-		if input() == 's':
-			engadir(indice)
+		print(_('*> Non existe, engadir? (s/n)'), end=': ')
+		if input() == ('s' or 'y'):
+			nome, tipo = engadir(indice)
+			valorar_aux(nome, tipo)
 
 #------------------------------------------------------------------------------------------------
 def pechar():
@@ -89,9 +146,10 @@ def menu():
 		print('\n-----------------------')
 		print(_('*> Elixe a opción:'))
 		print('-----------------------')
-		print(_('0 - Pechar o programa'))
-		print(_('1 - Engadir contido audiovisual'))
-		print(_('2 - Valorar contido'))
+		print(_('0. - Pechar o programa, sen gardar'))
+		print(_('0  - Pechar o programa, gardando'))
+		print(_('1  - Engadir contido audiovisual'))
+		print(_('2  - Valorar contido'))
 
 		print(_('Opción'), end=': ')
 		op = input()
@@ -104,17 +162,18 @@ def menu():
 	return op
 #------------------------------------------------------------------------------------------------
 if __name__=="__main__":
-	#_ = gettext.gettext
-	en = gettext.translation('caderno-viaxe', localedir='locales', languages=['en'])
-	en.install()
-	_ = en.gettext
+	_ = gettext.gettext
+	#en = gettext.translation('caderno-viaxe', localedir='locales', languages=['en'])
+	#en.install()
+	#_ = en.gettext
 
 	notas = u.cargar_json(__fnotas)
 	indice = u.cargar_json(__findice)
-	__ops = {'0': pechar,
+	__ops = {'0.': exit,
+			'0': pechar,
 			'1': partial(engadir, indice),
 			'2': partial(valorar, indice, notas)}
-	
+
 	while True:
 		__ops[menu()]()
 
--- utils.py
+++ utils.py
@@ -2,7 +2,7 @@
 #+ Autor:	Ran#
 # -*- coding: utf-8 -*-
 #+ Creado:	25/06/2019 21:37:29
-#+ Editado:	25/06/2019 21:37:29
+#+ Editado:	26/06/2019 16:10:44
 #------------------------------------------------------------------------------------------------
 import json
 from pathlib import Path
@@ -15,14 +15,22 @@ def cargar_json(fich):
 		open(fich, 'w').write('{}')
 		return json.loads(open(fich).read())
 #------------------------------------------------------------------------------------------------
+# función de gardado de ficheiros tipo json
 def gardar_json(fich, contido):
-	open(fich, 'w').write(json.dumps(contido, indent=1, sort_keys=False))
+	open(fich, 'w').write(json.dumps(contido, indent=1, sort_keys=False, ensure_ascii=False))
+#------------------------------------------------------------------------------------------------
+def crear_carp(carp):
+	if Path(carp).is_dir():
+		print('ok')
+	else:
+		Path(carp).mkdir(parents=True, exist_ok=True)
 #------------------------------------------------------------------------------------------------
 def epi_valido(epi):
 	if 'x' not in epi:
 		return False
 
 	temp, epi = epi.split('x')
+
 	if (temp.isdigit()) & (int(temp)>0) & (epi.isdigit()):
 		return True
 	else:
